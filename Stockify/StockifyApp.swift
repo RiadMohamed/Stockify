dcb64ddc5f4f2810aefb268e2544867f22d9bad9
Riad Mohamed <riaduk1@gmail.com>
2021-01-13 01:54:55 +0200

    Initial Commit

---
+++ Stockify/StockifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockifyApp.swift
+//  Stockify
+//
+//  Created by Riad Mohamed on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct StockifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
