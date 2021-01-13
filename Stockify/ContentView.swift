66766dc2552867853a4f518cc2e9136cbfb2e73d
Riad Mohamed <riaduk1@gmail.com>
2021-01-14 01:06:29 +0200

    Setting up the UI with SwiiftUI

--- Stockify/ContentView.swift
+++ Stockify/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var searchTerm: String = ""
+    
+    init() {
+        UINavigationBar.appearance().backgroundColor = .black
+        UINavigationBar.appearance().largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        UITableView.appearance().backgroundColor = .black
+        UITableViewCell.appearance().backgroundColor = .black
+    }
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack(alignment: .leading) {
+                
+                Color.black
+                Text("January 14th 2021")
+                    .font(.custom("Arial", size: 32))
+                    .fontWeight(.bold)
+                    .foregroundColor(.gray)
+                    .padding(EdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 0))
+                    .offset(y: -400)
+                
+                SearchView(searchTerm: $searchTerm)
+                    .offset(y: 150)
+                
+                
+            }
+                .navigationBarTitle("Stocks")
+        }.edgesIgnoringSafeArea(Edge.Set(.bottom))
     }
 }
 
